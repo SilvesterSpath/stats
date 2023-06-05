3637e20899cdc611c06420b8d48e58378a2ec59e
SilvesterSpath <spath.sz@gmail.com>
2023-06-05 14:36:39 +0200

    man united

--- build/index.js
+++ build/index.js
@@ -4,9 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs_1 = __importDefault(require("fs"));
-const matches = fs_1.default.readFileSync('./football.csv', {
+const matches = fs_1.default
+    .readFileSync('./football.csv', {
     encoding: 'utf-8',
-});
-const matches2 = matches.split('\n');
-const matches3 = matches2.map((item) => item.split(','));
-console.log(matches3);
+})
+    .split('\n')
+    .map((item) => item.split(','));
+// with this, it is more clear, but not entirely
+const homeWin = 'H';
+const awayWin = 'A';
+// this is also necessary
+const draw = 'D';
+let manUnitedWins = 0;
+for (const item of matches) {
+    if ((item[1] === 'Man United' && item[5] === homeWin) ||
+        (item[2] === 'Man United' && item[5] === awayWin)) {
+        manUnitedWins++;
+    }
+}
+console.log(`Man United won ${manUnitedWins} games`);
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,27 @@
 import fs from 'fs';
 
-const matches = fs.readFileSync('./football.csv', {
-  encoding: 'utf-8',
-});
+const matches = fs
+  .readFileSync('./football.csv', {
+    encoding: 'utf-8',
+  })
+  .split('\n')
+  .map((item: string): string[] => item.split(','));
 
-const matches2 = matches.split('\n');
-const matches3 = matches2.map((item) => item.split(','));
+// with this, it is more clear, but not entirely
+const homeWin = 'H';
+const awayWin = 'A';
+// this is also necessary
+const draw = 'D';
 
-console.log(matches3);
+let manUnitedWins = 0;
+
+for (const item of matches) {
+  if (
+    (item[1] === 'Man United' && item[5] === homeWin) ||
+    (item[2] === 'Man United' && item[5] === awayWin)
+  ) {
+    manUnitedWins++;
+  }
+}
+
+console.log(`Man United won ${manUnitedWins} games`);
