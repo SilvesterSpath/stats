d4f2e280b8be521f2efd51c9e80cacd0330b252e
SilvesterSpath <spath.sz@gmail.com>
2023-06-06 13:49:43 +0200

    CscFileReader read method

--- src/CsvFileReader.ts
+++ src/CsvFileReader.ts
@@ -1,4 +1,15 @@
+import fs from 'fs';
+
 export class CsvFileReader {
   data: string[][] = [];
   constructor(public filename: string) {}
+
+  read(): void {
+    this.data = fs
+      .readFileSync(this.filename, {
+        encoding: 'utf-8',
+      })
+      .split('\n')
+      .map((item: string): string[] => item.split(','));
+  }
 }
