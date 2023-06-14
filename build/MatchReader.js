563af4ea0342d0c0e0b2287ca510df6f70956ce7
SilvesterSpath <spath.sz@gmail.com>
2023-06-14 06:18:30 +0200

    MatchReader refactor

--- build/MatchReader.js
+++ build/MatchReader.js
@@ -1,19 +1,10 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.MatchReader = void 0;
-const CsvFileReader_1 = require("./CsvFileReader");
-const utils_1 = require("./utils");
-class MatchReader extends CsvFileReader_1.CsvFileReader {
-    mapRow(row) {
-        return [
-            (0, utils_1.changeDateFormat)(row[0]),
-            row[1],
-            row[2],
-            +row[3],
-            +row[4],
-            row[5],
-            row[6],
-        ];
+class MatchReader {
+    constructor(reader) {
+        this.reader = reader;
     }
+    load() { }
 }
 exports.MatchReader = MatchReader;
